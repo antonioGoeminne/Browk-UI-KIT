b2645fae2dda92f3bdd17a33ee5038cdf274187f
antonionicolas___ <antonionicolas3003@gmail.com>
2024-02-11 21:44:52 -0300

    FEAT: adding notification in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
-
-
-import { Badge, Button } from "@ui/index-server";
+import { Badge, Button, Notification } from "@ui/index-server";
 import { WrapperClients } from "../ui/wrapper-clients";
 import { Island } from "@ui/island/Island";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button label="Кнопка"  />
-      <Button label="Кнопка" variant={"secondary"} />
+      <div className="flex justify-around w-full">
+        <div className="flex flex-col gap-10">
+          <Button label="Кнопка" />
+          <Button label="Кнопка" variant={"secondary"} />
 
-      <Badge label="Your client"  />
-      <Badge label="Your client" variant={"secondary"} />
-      <Badge label="Your client" variant={"white"} />
-      <Island head="Head" description="Description" badgeText="Your text"/>
-      <WrapperClients />
+          <Badge label="Your client" />
+          <Badge label="Your client" variant={"secondary"} />
+          <Badge label="Your client" variant={"white"} />
+        </div>
+        <div className="flex flex-col gap-10">
+          <Island head="Head" description="Description" badgeText="Your text" />
+          <Notification head="Head" description="Description" withButton />
+          <WrapperClients />
+        </div>
+      </div>
     </main>
   );
 }
--- src/ui/index-server.tsx
+++ src/ui/index-server.tsx
@@ -1,2 +1,3 @@
-export * from './button/Button'
-export * from './badge/Badge'
+export * from "./button/Button";
+export * from "./badge/Badge";
+export * from "./notification/Notification";
